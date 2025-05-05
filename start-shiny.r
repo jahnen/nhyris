7dc7fea88f18b1a8b9149fd645b457b4de8992a5
Jinhwan Kim <hwanistic@gmail.com>
2025-05-05 22:29:33 +0900

    fix: split port
    electron & shiny used same 1124 port;
    electron will use 1124. shiny will use 1126

--- start-shiny.r
+++ start-shiny.r
@@ -4,9 +4,12 @@
 # Parameters are supplied using environment variables
 assign(".lib.loc", Sys.getenv("R_LIB_PATHS"), envir = environment(.libPaths))
 
+# Electron use 1124 port
+# so Shiny should not use 1124 port
+
 shiny::runApp(
   Sys.getenv("RE_SHINY_PATH"),
   host = "127.0.0.1",
   launch.browser = FALSE,
-  port = 1124
+  port = 1126
 )
