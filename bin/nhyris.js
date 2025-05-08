47908ba42cb2e07cc00ac8188d716bf70120d332
Jinhwan Kim <hwanistic@gmail.com>
2025-05-08 22:02:38 +0900

    feat: run command

--- bin/nhyris.js
+++ bin/nhyris.js
@@ -3,11 +3,13 @@
 import { Command } from "commander";
 import { initCommand } from "../commands/init.js";
 import { buildCommand } from "../commands/build.js";
+import { runCommand } from "../commands/run.js";
 
 const program = new Command();
 program.name("nhyris").description("CLI for Shiny Electron apps");
 
 program.addCommand(initCommand);
 program.addCommand(buildCommand);
+program.addCommand(runCommand);
 
 program.parse();
