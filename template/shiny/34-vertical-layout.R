4b6e7a0a3e4072de88500aee62018cb5ad2730ca
Jinhwan Kim <hwanistic@gmail.com>
2025-06-07 13:54:15 +0900

    feat: ex34

---
+++ template/shiny/34-vertical-layout.R
@@ -0,0 +1,18 @@
+ui <- fluidPage(
+  verticalLayout(
+    titlePanel("Vertical layout example"),
+    plotOutput("plot1"),
+    wellPanel(
+      sliderInput("n", "Number of points", 10, 200,
+                  value = 50, step = 10)
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$plot1 <- renderPlot({
+    plot(x = rnorm(input$n), y = rnorm(input$n))
+  })
+}
+
+shinyApp(ui = ui, server = server)
